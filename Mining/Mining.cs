88d9274bb92cfc13cdefdc80c06c4287ec2d9194
blaxxun <blaxxun@blaxxun.blaxxun>
2022-03-18 23:37:05 +0100

    add config options and smaller bugfixes

--- Mining/Mining.cs
+++ Mining/Mining.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Reflection;
 using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using JetBrains.Annotations;
+using ServerSync;
 using SkillManager;
 using UnityEngine;
 
@@ -12,9 +14,41 @@ namespace Mining;
 public class Mining : BaseUnityPlugin
 {
 	private const string ModName = "Mining";
-	private const string ModVersion = "1.0.0";
+	private const string ModVersion = "1.1.0";
 	private const string ModGUID = "org.bepinex.plugins.mining";
 
+	private static readonly ConfigSync configSync = new(ModGUID) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+	private static ConfigEntry<Toggle> serverConfigLocked = null!;
+	private static ConfigEntry<float> miningDamageFactor = null!;
+	private static ConfigEntry<float> miningItemYieldFactor = null!;
+	private static ConfigEntry<int> explosionMinimumLevel = null!;
+	private static ConfigEntry<float> explosionChance = null!;
+	private static ConfigEntry<float> experienceGainedFactor = null!;
+
+	private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
+	{
+		ConfigEntry<T> configEntry = Config.Bind(group, name, value, description);
+
+		SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+		syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+		return configEntry;
+	}
+
+	private ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+
+	private enum Toggle
+	{
+		On = 1,
+		Off = 0
+	}
+
+	private class ConfigurationManagerAttributes
+	{
+		[UsedImplicitly] public bool? ShowRangeAsPercent;
+	}
+	
 	private static readonly Skill mining = new("Mining", "mining.png");
 
 	public void Awake()
@@ -22,7 +56,17 @@ public class Mining : BaseUnityPlugin
 		mining.Description.English("Increases damage done while mining and item yield from ore deposits.");
 		mining.Name.German("Bergbau");
 		mining.Description.German("Erhöht den Schaden während Bergbau-Aktivitäten und erhöht die Ausbeute von Erzvorkommen.");
-		mining.Configurable = true;
+		mining.Configurable = false;
+		
+		serverConfigLocked = config("1 - General", "Lock Configuration", Toggle.On, "If on, the configuration is locked and can be changed by server admins only.");
+		configSync.AddLockingConfigEntry(serverConfigLocked);
+		miningDamageFactor = config("2 - Mining", "Mining Damage Factor", 3f, new ConfigDescription("Mining damage factor at skill level 100.", new AcceptableValueRange<float>(1f, 10f)));
+		miningItemYieldFactor = config("2 - Mining", "Mining Yield Factor", 2f, new ConfigDescription("Mining yield factor at skill level 100.", new AcceptableValueRange<float>(1f, 5f)));
+		explosionMinimumLevel = config("2 - Mining", "Mining Explosion Level Requirement", 50, new ConfigDescription("Minimum required skill level to have a chance for the deposit to explode. 0 is disabled.", new AcceptableValueRange<int>(0, 100), new ConfigurationManagerAttributes { ShowRangeAsPercent = false }));
+		explosionChance = config("2 - Mining", "Mining Explosion Chance", 1f, new ConfigDescription("Mining explosion chance at skill level 100.", new AcceptableValueRange<float>(0, 100)));
+		experienceGainedFactor = config("3 - Other", "Skill Experience Gain Factor", 1f, new ConfigDescription("Factor for experience gained for the mining skill.", new AcceptableValueRange<float>(0.01f, 5f)));
+		experienceGainedFactor.SettingChanged += (_, _) => mining.SkillGainFactor = experienceGainedFactor.Value;
+		mining.SkillGainFactor = experienceGainedFactor.Value;
 
 		Assembly assembly = Assembly.GetExecutingAssembly();
 		Harmony harmony = new(ModGUID);
@@ -57,7 +101,7 @@ public class Mining : BaseUnityPlugin
 		{
 			if (__instance.m_character is Player player)
 			{
-				hitData.m_damage.m_pickaxe *= 1 + player.GetSkillFactor("Mining") * 2;
+				hitData.m_damage.m_pickaxe *= 1 + player.GetSkillFactor("Mining") * (miningDamageFactor.Value - 1);
 			}
 		}
 	}
@@ -77,7 +121,7 @@ public class Mining : BaseUnityPlugin
 			List<GameObject> tmp = new();
 			foreach (GameObject item in __result)
 			{
-				float amount = 1 + SetMiningFlag.MiningFactor;
+				float amount = 1 + SetMiningFlag.MiningFactor * (miningItemYieldFactor.Value - 1);
 
 				for (int num = Mathf.FloorToInt(amount + Random.Range(0f, 1f)); num > 0; --num)
 				{
@@ -97,14 +141,19 @@ public class Mining : BaseUnityPlugin
 
 		private static bool Prefix(MineRock5 __instance, HitData hit)
 		{
+			if (hit.GetAttacker() is not Player player)
+			{
+				return true;
+			}
+			
 			IsMining = true;
-			MiningFactor = hit.GetAttacker()?.m_nview.GetZDO()?.GetFloat("Mining Skill Factor") ?? 0;
+			MiningFactor = player.m_nview.GetZDO()?.GetFloat("Mining Skill Factor") ?? 0;
 
 			if (hit.m_toolTier >= __instance.m_minToolTier && hit.m_damage.m_pickaxe > 0 && __instance.m_nview.IsValid() && __instance.m_nview.IsOwner())
 			{
-				hit.GetAttacker().m_nview.InvokeRPC("Mining IncreaseSkill", 1);
+				player.m_nview.InvokeRPC("Mining IncreaseSkill", 1);
 
-				if (MiningFactor > 0.5 && Random.Range(0f, 1f) <= (MiningFactor - 0.4) * 0.02)
+				if (explosionMinimumLevel.Value > 0 && MiningFactor >= explosionMinimumLevel.Value / 100f && Random.Range(0f, 1f) <= (MiningFactor - (explosionMinimumLevel.Value - 10) / 100f) / (1 - (explosionMinimumLevel.Value - 10) / 100f) * explosionChance.Value / 100f)
 				{
 					for (int i = 0; i < __instance.m_hitAreas.Count; ++i)
 					{
